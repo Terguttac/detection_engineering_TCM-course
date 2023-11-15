b33a2a76e24b912e6896e78fbdf0c0429db032d0
Sean Dixon <72320127+Terguttac@users.noreply.github.com>
2023-11-15 15:29:22 -0800

    Added Scripts and Detections

---
+++ development/validation.py
@@ -0,0 +1,33 @@
+import tomllib
+import sys
+import os
+
+for root, dirs, files in os.walk('detections/'):
+	for file in files:
+		if file.endswith('.toml'):
+			full_path = os.path.join(root, file)
+			with open(full_path,'rb') as toml:
+				alert = tomllib.load(toml)	
+
+				if alert['rule']['type'] == 'query': # query based alert
+					required_fields = ['description','name','rule_id','risk_score','severity','type','query']
+				elif alert['rule']['type'] == 'eql': # event correlation alert
+					required_fields = ['description','name','rule_id','risk_score','severity','type','query', 'language']
+				elif alert['rule']['type'] == 'threshold': # threshold based alert
+					required_fields = ['description','name','rule_id','risk_score','severity','type','query', 'threshold']
+				else:
+					print('Invalid rule type:', alert['rule']['type'], 'in ', full_path)
+					break
+
+				present_fields = []
+				missing_fields = []
+
+				for field in alert['rule']:
+					present_fields.append(field)
+
+				for field in required_fields:
+					if field not in present_fields:
+						missing_fields.append(field)
+
+				if missing_fields:
+					print("The following fields do not exist in", file + ":", str(missing_fields))
